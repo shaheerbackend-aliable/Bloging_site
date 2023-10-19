287bb93bfabf8fd3ae6c0464fe62620a2d2c7587
Muhammad Ali <99584926+alibinsarwar@users.noreply.github.com>
2023-10-19 21:16:49 +0500

    fix css

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Blog Site</title>
     @vite('resources/css/app.scss')
+
     <link rel="stylesheet" href="style.scss" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
