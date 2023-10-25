323112434d9e1013cac92beaa0471a191e1cd40e
shaheerbackend-aliable <148327570+shaheerbackend-aliable@users.noreply.github.com>
2023-10-25 14:04:21 +0500

    Email Issue clear

--- resources/views/user/email.blade.php
+++ resources/views/user/email.blade.php
@@ -6,6 +6,6 @@
 <strong>Email: </strong>{{ $data->email }} <br>
 <strong>Phone: </strong>{{ $data->phone }} <br>
 <strong>Subject: </strong>{{ $data->subject }} <br>
-<strong>Message: </strong>{{ $data->user_query }} <br><br>
+<strong>Message: </strong>{{$data->message}} <br><br>
   
 Thank you!!
