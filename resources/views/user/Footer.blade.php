058b00ad584a27a0eb4b7569c7bccc345dfd89f7
A-khaliq-Frontend <148327183+frontend-aliable@users.noreply.github.com>
2023-10-26 14:17:04 +0500

    fonts

--- resources/views/user/Footer.blade.php
+++ resources/views/user/Footer.blade.php
@@ -1,83 +1,101 @@
 <footer class="footer-09">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-lg-3 mb-md-0 mb-4">
-						<h2 class="footer-heading d-flex">
-							<span class="icon d-flex align-items-center justify-content-center"><i class="ion-logo-ionic"></i></span>
-							About
-						</h2>
-						<div class="block-23 mb-3">
-              <ul>
-                <li><span class="icon ion-ios-pin"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                <li><a href="#"><span class="icon ion-ios-call"></span><span class="text">+2 392 3929 210</span></a></li>
-                <li><a href="#"><span class="icon ion-ios-send"></span><span class="text">info@yourdomain.com</span></a></li>
-              </ul>
-            </div>
-            <form action="#" class="subscribe-form">
-              <div class="form-group d-flex">
-                <input type="text" class="form-control rounded-left" placeholder="Enter email address">
-                <button type="submit" class="form-control submit rounded-right"><span class="sr-only">Submit</span><i class="ion-ios-send"></i></button>
-              </div>
-            </form>
-					</div>
-					<div class="col-md-6 col-lg-3 mb-md-0 mb-4">
-						<h2 class="footer-heading d-flex align-items-center"><span class="icon d-flex align-items-center justify-content-center"><i class="ion-ios-list-box"></i></span>Latest News</h2>
-						<div class="block-21 mb-4 d-flex">
-              <a class="img mr-4 rounded" style="background-image: url(images/image_1.jpg);"></a>
-              <div class="text">
-                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                <div class="meta">
-                  <div><a href="#"><span class="icon-calendar"></span> Oct. 16, 2019</a></div>
-                  <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                  <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                </div>
-              </div>
-            </div>
-            <div class="block-21 mb-4 d-flex">
-              <a class="img mr-4 rounded" style="background-image: url(images/image_2.jpg);"></a>
-              <div class="text">
-                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                <div class="meta">
-                  <div><a href="#"><span class="icon-calendar"></span> Oct. 16, 2019</a></div>
-                  <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                  <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                </div>
-              </div>
-            </div>
-					</div>
-					<div class="col-md-6 col-lg-3 mb-md-0 mb-4">
-						<h2 class="footer-heading d-flex align-items-center"><span class="icon d-flex align-items-center justify-content-center"><i class="ion-ios-document"></i></span>Information</h2>
-						<ul class="list-unstyled">
-	            <li><a href="#" class="py-1 d-block">About</a></li>
-	            <li><a href="#" class="py-1 d-block">Products</a></li>
-	            <li><a href="#" class="py-1 d-block">Blog</a></li>
-	            <li><a href="#" class="py-1 d-block">Contact</a></li>
-	            <li><a href="#" class="py-1 d-block">Help &amp; Support</a></li>
-	          </ul>
-					</div>
-					<div class="col-md-6 col-lg-3 mb-md-0 mb-4">
-						<h2 class="footer-heading d-flex align-items-center"><span class="icon d-flex align-items-center justify-content-center"><i class="fa fa-user"></i></span>Follow US</h2>
-						<div class="block-24">
-							<div class="row no-gutters ">
-								<div class="col-4 col-md-4 p-1 bg-dark">
-                    <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
-                   <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
-                    <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
-								</div>
-								
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row mt-5 pt-4 border-top">
-          <div class="col-md-6 col-lg-8">
-            <p class="copyright">
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved.</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-lg-3 footer-content">
+        <h2 class="footer-heading d-flex">
+          <span class="icon d-flex align-items-center justify-content-center"><i class="ion-logo-ionic"></i></span>
+          About
+        </h2>
+        <div class="block-23 mb-3">
+          <ul>
+            <li><span class="icon ion-ios-pin"></span><span class="text">203 Fake St. Mountain View, San Francisco,
+                California, USA</span></li>
+            <li><a href="#"><span class="icon ion-ios-call"></span><span class="text">+2 392 3929 210</span></a></li>
+            <li><a href="#"><span class="icon ion-ios-send"></span><span class="text">info@yourdomain.com</span></a>
+            </li>
+          </ul>
+        </div>
+        <form action="#" class="subscribe-form">
+          <div class="form-group d-flex">
+            <input type="text" class="form-control rounded-left" placeholder="Enter email address">
+            <button type="submit" class="form-control submit rounded-right"><span class="sr-only">Submit</span><i
+                class="fa-light fa-envelope"></i></button>
           </div>
-          <div class="col-md-6 col-lg-4 text-md-right">
-          	<p class="copyright">Made By <i class="ion-ios-heart" aria-hidden="true"></i> by <a href="#" target="_blank">Acute Dev</a>
-</p>
+        </form>
+      </div>
+      <div class="col-md-6 col-lg-3 footer-content footer-contentnt">
+        <h2 class="footer-heading d-flex align-items-center"><span
+            class="icon d-flex align-items-center justify-content-center"><i class="ion-ios-list-box"></i></span>Latest
+          News</h2>
+        <div class="block-21 mb-4 d-flex">
+          <a class="img mr-4 rounded" style="background-image: url(images/image_1.jpg);"></a>
+          <div class="text">
+            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
+            <div class="meta">
+              <div><a href="#"><span class="icon-calendar"></span> Oct. 16, 2019</a></div>
+              <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+              <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+            </div>
           </div>
         </div>
-			</div>
-		</footer>
+        <div class="block-21 mb-4 d-flex">
+          <a class="img mr-4 rounded" style="background-image: url(images/image_2.jpg);"></a>
+          <div class="text">
+            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
+            <div class="meta">
+              <div><a href="#"><span class="icon-calendar"></span> Oct. 16, 2019</a></div>
+              <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+              <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 footer-content">
+        <h2 class="footer-heading d-flex align-items-center"><span
+            class="icon d-flex align-items-center justify-content-center"><i
+              class="ion-ios-document"></i></span>Information</h2>
+        <ul class="list-unstyled">
+          <li><a href="#" class="py-1 d-block">About</a></li>
+          <li><a href="#" class="py-1 d-block">Products</a></li>
+          <li><a href="#" class="py-1 d-block">Blog</a></li>
+          <li><a href="#" class="py-1 d-block">Contact</a></li>
+          <li><a href="#" class="py-1 d-block">Help &amp; Support</a></li>
+        </ul>
+      </div>
+      <div class="col-md-6 col-lg-3 footer-content">
+        <h2 class="footer-heading d-flex align-items-center"><span
+            class="icon d-flex align-items-center justify-content-center"><i class="fa fa-user"></i></span>Follow US
+        </h2>
+        <div class="col-md-4 d-flex">
+          <div class="social-media-footer">
+            <p class="mb-0 d-flex">
+              <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i
+                    class="sr-only">Facebook</i></span></a>
+              <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i
+                    class="sr-only">Twitter</i></span></a>
+              <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i
+                    class="sr-only">Instagram</i></span></a>
+              <!-- <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a> -->
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row  px-4 pt-4 border-top">
+  <div class="col-md-6 col-lg-8">
+    <p class="copyright text-light">
+      Copyright &copy;
+      <script>document.write(new Date().getFullYear());</script> All rights reserved.
+    </p>
+  </div>
+  <div class="col-md-6 col-lg-4 text-md-right">
+    <p class="copyright text-light">Made By <i class="ion-ios-heart" aria-hidden="true"></i> by <a href="#"
+    target="_blank">Acute Dev</a>
+  </p>
+</div>
+</div>
+  </div>
+</footer>
