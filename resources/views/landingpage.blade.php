70f8da9d63d7cb631e6a8ac8d1e7b50a3ec27b02
shaheerbackend-aliable <148327570+shaheerbackend-aliable@users.noreply.github.com>
2023-11-29 02:42:48 +0500

    landing done

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -15,7 +15,7 @@
   <!-- start topbar content -->
   <nav class="navbar navbar-expand-lg navbar-dark ">
     <div class="container-fluid">
-      <a class="navbar-brand " href="#">Navbar</a>
+      <a class="navbar-brand " href="\">BabaJie</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
